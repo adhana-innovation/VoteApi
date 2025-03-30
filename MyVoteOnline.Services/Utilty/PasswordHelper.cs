0ba00b027cdd5ba2c057ac21f3095512513606e6
adhana-innovation <adhanainnovation@gmail.com>
2025-03-30 22:15:11 +0530

    Update PasswordHelper.cs

--- MyVoteOnline.Services/Utilty/PasswordHelper.cs
+++ MyVoteOnline.Services/Utilty/PasswordHelper.cs
@@ -4,6 +4,7 @@
 	{
 		//this is for convert password into hashpassword
 		public static string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
+		
 		//this is for macth password at the time of login 
 		public static bool VerifyPassword(string password,string hashedpassword)
 		{
